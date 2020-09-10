b54a9fb4df42597d4065edc4806688bf634b9b68
Adhil-Mohammed-P-N <adhilmohammedpn@gmail.com>
2020-09-10 17:50:23 +0530

    react router is added

---
+++ src/Checkout.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./checkout.css";
+
+function Checkout() {
+  return (
+    <div className="checkout">
+      <div className="checkout__left">
+        <img
+          className="checkout__ad"
+          src="https://images-eu.ssl-images-amazon.com/images/G/31/img19/Wireless/Jupiter19/Launches/new/Store-Page_PC_CAT_Headers_GPS_1500x250._CB437086357_.jpg"
+          alt=""
+        />
+        <div>
+          <h1 className="checkout__title">Your Shopping Basket</h1>
+          {/*  BasketItem */}
+          {/*  BasketItem */}
+          {/*  BasketItem */}
+        </div>
+      </div>
+      <div className="checkout__right">
+        <h2>subtotal will be here</h2>
+      </div>
+    </div>
+  );
+}
+
+export default Checkout;
