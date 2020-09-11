3b51c5a7cd952ccccfd4689e7b1ba8d7abd55fbe
Adhil-Mohammed-P-N <adhilmohammedpn@gmail.com>
2020-09-11 18:05:53 +0530

    add to masket

--- src/Stateprovider.js
+++ src/Stateprovider.js
@@ -2,10 +2,10 @@ import React, { createContext, useContext, useReducer } from "react";
 //preparing the datalayer
 export const StateContext = createContext();
 //wrap our app and provide the data layer
-export const StateProvider = ({ reducer, initialState, children }) => {
+export const StateProvider = ({ reducer, initialState, children }) => (
   <StateContext.Provider value={useReducer(reducer, initialState)}>
     {children}
-  </StateContext.Provider>;
-};
+  </StateContext.Provider>
+);
 //pull information from data layer
 export const useStateValue = () => useContext(StateContext);
